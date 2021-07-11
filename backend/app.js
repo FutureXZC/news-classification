3dc33d3cbe7f2d295bc34ae20f54cfcd387c8160
FutureXZC <1073739700@qq.com>
2021-07-11 17:10:23 +0800

    [+] 后端引入前端的打包文件

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,7 @@ var app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'dist')))
 app.set('view engine', 'ejs')
 
 app.use(logger('dev'))
