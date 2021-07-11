dc987619bd6f4b5107ea14c1083ab28d9e2a198a
FutureXZC <1073739700@qq.com>
2021-07-11 14:10:29 +0800

    [+] 完善进度条，改进布局，删除冗余数据

--- frontend/src/api/apis.js
+++ frontend/src/api/apis.js
@@ -1,3 +1,8 @@
+/**
+ * @abstract 调用新闻分类接口，对新闻文本进行分析。mode = 1：根据文件分类多条新闻；mode = 0：根据表单分类单条新闻
+ * @param {用于分析的数据} data 多条新闻：{ filename: 文件名, mode: 1 }
+ *                             单条新闻：{ title: 新闻标题, content: 新闻正文内容, mode: 0 }
+ */
 export function cls(data) {
   return fetch("http://localhost:3000/process/cls", {
     method: "post",
@@ -8,6 +13,10 @@ export function cls(data) {
   });
 }
 
+/**
+ * @abstract 调用文件下载接口，下载的文件名即为上传时的文件名，下载的文件将包含已经标记好的新闻类别信息
+ * @param {filename} filename 要下载的文件名
+ */
 export function download(filename) {
   window.location.href = `http://localhost:3000/multer/download?filename=${filename}`;
 }
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,7 +9,6 @@ import "element-ui/lib/theme-chalk/index.css";
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
 
-/* eslint-disable no-new */
 new Vue({
   el: "#app",
   router,
